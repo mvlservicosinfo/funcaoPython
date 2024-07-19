70d0e01cfa0d49bafc82aa6084c797fff4a5e2f5
mvlservicosinfo <60225498+mvlservicosinfo@users.noreply.github.com>
2024-07-19 11:21:39 -0300

    "FuncaoAlternaValor"

--- pythonProject2/Funcoes/main.py
+++ pythonProject2/Funcoes/main.py
@@ -1,6 +1,10 @@
 from Funcoes.F01 import F001
+from Funcoes.F02 import AlternaSinal
 
 if __name__ == '__main__':
-    nome = input("Entre com seu nome: ")
+   """ nome = input("Entre com seu nome: ")
     f1 = F001(nome)
     print(f"O seu nome é: {f1.saudacao()}")
+    """
+   f2=AlternaSinal(10)
+   f2.valor1()
