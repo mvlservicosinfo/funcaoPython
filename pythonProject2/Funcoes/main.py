eb85c33b3dfaf0a82463eb0132f3f8be34a36022
mvlservicosinfo <60225498+mvlservicosinfo@users.noreply.github.com>
2024-07-19 11:33:00 -0300

    "Funcao Calcula Area do Triangulo"

--- pythonProject2/Funcoes/main.py
+++ pythonProject2/Funcoes/main.py
@@ -1,10 +1,14 @@
 from Funcoes.F01 import F001
 from Funcoes.F02 import AlternaSinal
+from Funcoes.F03_calc_area_triangulo import Triangulo
 
 if __name__ == '__main__':
    """ nome = input("Entre com seu nome: ")
     f1 = F001(nome)
     print(f"O seu nome é: {f1.saudacao()}")
-    """
+    
    f2=AlternaSinal(10)
    f2.valor1()
+   """
+   t3 =Triangulo(5,4)
+   print(f" A área do Triangulo é: {t3.area()}")
